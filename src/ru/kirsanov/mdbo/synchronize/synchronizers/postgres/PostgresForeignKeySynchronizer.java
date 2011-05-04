e8472e52b80fc3dad35f55065db976c9da1b98f5
lqip32 <lqip32@desktop.(none)>
2011-05-05 01:23:04 +0600

    Добавлена синхронизация foreignkey для postgres

---
+++ src/ru/kirsanov/mdbo/synchronize/synchronizers/postgres/PostgresForeignKeySynchronizer.java
@@ -0,0 +1,58 @@
+package ru.kirsanov.mdbo.synchronize.synchronizers.postgres;
+
+import ru.kirsanov.mdbo.metamodel.constraint.ForeignKey;
+import ru.kirsanov.mdbo.metamodel.entity.*;
+import ru.kirsanov.mdbo.metamodel.exception.ColumnNotFoundException;
+import ru.kirsanov.mdbo.metamodel.exception.TableNotFound;
+import ru.kirsanov.mdbo.synchronize.exception.ModelSynchronizerNotFound;
+import ru.kirsanov.mdbo.synchronize.synchronizers.IEntitySynchronizer;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class PostgresForeignKeySynchronizer implements IEntitySynchronizer {
+    private Connection connection;
+
+    public PostgresForeignKeySynchronizer(Connection connection) {
+        this.connection = connection;
+    }
+
+    public Model execute(Model model) throws ModelSynchronizerNotFound, SQLException, TableNotFound, ColumnNotFoundException {
+        if (!(model instanceof PostgresModel)) throw new ModelSynchronizerNotFound();
+        PreparedStatement selectInformationFromSysTable = connection
+                .prepareStatement("SELECT\n" +
+                        "    tc.constraint_name, tc.constraint_schema, tc.table_name, kcu.column_name, \n" +
+                        "    ccu.table_name AS foreign_table_name,\n" +
+                        "    ccu.column_name AS foreign_column_name \n" +
+                        "FROM \n" +
+                        "    information_schema.table_constraints AS tc \n" +
+                        "    JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name\n" +
+                        "    JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name\n" +
+                        "WHERE constraint_type = 'FOREIGN KEY';");
+
+        List<ISchema> schemas = model.getSchemas();
+        ResultSet resultSetOfSchema = selectInformationFromSysTable.executeQuery();
+        while(resultSetOfSchema.next()){
+            String constraintName = resultSetOfSchema.getString("CONSTRAINT_NAME").toLowerCase();
+            String schemaName = resultSetOfSchema.getString("CONSTRAINT_SCHEMA").toLowerCase();
+            String sourceTableName = resultSetOfSchema.getString("TABLE_NAME").toLowerCase();
+            String sourceColumnName = resultSetOfSchema.getString("COLUMN_NAME").toLowerCase();
+            String targetTableName = resultSetOfSchema.getString("FOREIGN_TABLE_NAME").toLowerCase();
+            String targetColumnName = resultSetOfSchema.getString("FOREIGN_COLUMN_NAME").toLowerCase();
+           for (ISchema schema:schemas){
+               if (schema.getName().equals(schemaName)){
+                   ITable sourceTable = schema.getTable(sourceTableName);
+                   ITable targetTable = schema.getTable(targetTableName);
+                   ForeignKey foreignKey = schema.createForeignKey(constraintName, sourceTable, targetTable);
+                   foreignKey.addColumnMapping(sourceTable.getColumn(sourceColumnName),targetTable.getColumn(targetColumnName));
+                   break;
+               }
+           }
+        }
+        connection.setAutoCommit(false);
+        return model;
+    }
+}
---
+++ test/ru/kirsanov/mdbo/synchronize/synchronizers/postgres/PostgersForeignKeySynchronizerTest.java
@@ -0,0 +1,88 @@
+package ru.kirsanov.mdbo.synchronize.synchronizers.postgres;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import ru.kirsanov.mdbo.metamodel.constraint.ForeignKey;
+import ru.kirsanov.mdbo.metamodel.datatype.DataType;
+import ru.kirsanov.mdbo.metamodel.datatype.SimpleDatatype;
+import ru.kirsanov.mdbo.metamodel.entity.*;
+import ru.kirsanov.mdbo.metamodel.exception.ColumnAlreadyExistsException;
+import ru.kirsanov.mdbo.metamodel.exception.ColumnNotFoundException;
+import ru.kirsanov.mdbo.synchronize.utility.ConnectionData;
+import ru.kirsanov.mdbo.synchronize.utility.ConnectionManger;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.Assert.assertEquals;
+
+public class PostgersForeignKeySynchronizerTest {
+    private PostgresModel testModel;
+    private ConnectionManger cm;
+
+    @Before
+    public void setUp() throws ColumnAlreadyExistsException, ColumnNotFoundException, SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+       cm = new ConnectionManger(new ConnectionData("localhost", "test", "postgresql", "lqip32", "4f3v6"));
+        testModel = new PostgresModel("testbase");
+    }
+
+    @Test
+    public void executeTest() throws Throwable {
+        ConnectionManger conn = new ConnectionManger(new ConnectionData("localhost", "test", "postgresql", "lqip32", "4f3v6"));
+        Statement statement = null;
+        try {
+            statement = conn.getConnection().createStatement();
+            statement
+                    .executeUpdate("DROP TABLE IF EXISTS childs;");
+
+            statement
+                    .executeUpdate("DROP TABLE IF EXISTS parents;");
+            statement
+                    .executeUpdate("CREATE TABLE parents (id INTEGER NOT NULL,\n" +
+                            "                     PRIMARY KEY (id)\n" +
+                            ") \n");
+            statement
+                    .executeUpdate("CREATE TABLE childs (id INT, parent_id INT\n" +
+                            "                     REFERENCES parents(id)\n" +
+                            "                      ON DELETE CASCADE\n" +
+                            ")");
+            Model model = new PostgresModel("testbase");
+            ISchema schema = testModel.createSchema("public");
+            ITable parentsTable = new Table("parents");
+            schema.addTable(parentsTable);
+            DataType intDataType = new SimpleDatatype("integer", 32);
+            IColumn parentsIdColumn = parentsTable.createColumn("id", intDataType);
+            parentsIdColumn.setNullable(false);
+            ITable childsTable = new Table("childs");
+            schema.addTable(childsTable);
+            IColumn childsIdColumn = childsTable.createColumn("id", intDataType);
+            childsIdColumn.setNullable(true);
+            IColumn childsParentIdColumn = childsTable.createColumn("parent_id", intDataType);
+            childsParentIdColumn.setNullable(true);
+            PostgresTableSynchronizer postgresTableSynchronizer = new PostgresTableSynchronizer(cm.getConnection());
+            PostgresForeignKeySynchronizer postgresForeignKeySynchronizer = new PostgresForeignKeySynchronizer(cm.getConnection());
+            Model synchronizeModel = postgresForeignKeySynchronizer.execute(postgresTableSynchronizer.execute(model));
+            ForeignKey fk = schema.createForeignKey("childs_parent_id_fkey", childsTable, parentsTable);
+            fk.addColumnMapping(childsParentIdColumn, parentsIdColumn);
+            assertEquals(testModel,synchronizeModel);
+        } finally {
+            statement.close();
+        }
+
+    }
+
+    @After
+    public void tearDown
+            () throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+        ConnectionManger conn = new ConnectionManger(new ConnectionData("localhost", "test", "postgresql", "lqip32", "4f3v6"));
+        Statement statement = conn.getConnection().createStatement();
+        statement
+                .executeUpdate("DROP TABLE IF EXISTS childs;");
+        statement
+                .executeUpdate("DROP TABLE IF EXISTS parents;");
+        statement.close();
+        conn.getConnection().close();
+    }
+
+}
